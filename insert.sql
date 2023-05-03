acd8c9e47df9570b8bb7d9141eacb8ddfc8cfa90
Louis Richard <113582793+LouisRichard8@users.noreply.github.com>
2023-05-03 21:20:05 +0200

    Modification du MCD et de la création des tables + insert complet

--- insert.sql
+++ insert.sql
@@ -5,8 +5,12 @@ delete from realise;
 delete from joue;
 delete from compose;
 delete from interprete;
+delete from scenarise;
 delete from contenu_type;
 delete from mot_clef;
+delete from achat;
+delete from loue;
+delete from saison;
 delete from contenu;
 delete from theme;
 delete from classification;
@@ -42,10 +46,7 @@ insert into theme(nomtheme)
 insert into contenu (titre, description, duree, pays, anneeProd, classification, themeprincipal) 
     select d.title, 
            d.description, 
-           (case d.type 
-                when 'TV Show' then null 
-                else to_number(substr(d.duration, 1, instr(d.duration, ' ')-1)) 
-            end), 
+           to_number(substr(d.duration, 1, instr(d.duration, ' ')-1)) , 
            (case  
                 when INSTR(d.country, ', ') < 1 then d.country 
                 else SUBSTR(d.country, 1, INSTR(d.country, ', ')-1) 
@@ -74,6 +75,34 @@ insert into contenu (titre, description, duree, pays, anneeProd, classification,
           vt.show_id = d.show_id              
 ;
 
+insert into contenu_type (refContenu, nomType)
+    select distinct c.ref, (case 
+                    when ((d.type = 'TV Show' and (d.listed_in like '%Kid%' or d.listed_in like '%Teen%')) or (d.type = 'Movie' and (d.listed_in like '%Children%' or d.listed_in like '%Family%'))) then 'Jeunesse' 
+                    when (d.type = 'TV Show' ) then 'Series'
+                    when (d.type = 'Movie' and (d.listed_in like '%Stand-Up Comedy%' or d.listed_in like '%Stand-Up Comedy & Talk Shows%')) then 'Divertissement'
+                    else 'Cinéma'
+                  end)
+    from contenu c, datanetflouze d
+    where c.titre = d.title
+;
+
+begin
+    for serie in (select * 
+                from contenu c, contenu_type ct
+                where ct.refContenu = c.ref and 
+                    ct.nomtype = 'Series') 
+    loop
+        for nb_saison in 1..(serie.duree)
+        loop
+            insert into contenu (titre, description, duree, pays, anneeProd, classification, themeprincipal)
+            values (serie.titre||' - Saison '||nb_saison, serie.description, null, serie.pays, serie.anneeProd, serie.classification, serie.themeprincipal);
+            insert into saison (serie, saison)
+            values (serie.ref, (select ref from contenu where titre = serie.titre||' - Saison '||nb_saison));
+        end loop;
+    end loop;
+end;
+/
+
 insert into mot_clef (refContenu, idTheme)
     select c.ref, t.idtheme
     from datanetflouze d, contenu c, theme t, v_thematic vt
@@ -81,17 +110,6 @@ insert into mot_clef (refContenu, idTheme)
           vt.pos > 1 and
           vt.show_id = d.show_id and 
           c.titre = d.title           
-;
-
-insert into contenu_type (refContenu, nomType)
-    select distinct c.ref, (case 
-                    when ((d.type = 'TV Show' and (d.listed_in like '%Kid%' or d.listed_in like '%Teen%')) or (d.type = 'Movie' and (d.listed_in like '%Children%' or d.listed_in like '%Family%'))) then 'Jeunesse' 
-                    when (d.type = 'TV Show' ) then 'Series'
-                    when (d.type = 'Movies' and (d.listed_in like '%Stand-Up Comedy%' or d.listed_in like '%Stand-Up Comedy & Talk Shows%')) then 'Divertissement'
-                    else 'Cinéma'
-                  end)
-    from contenu c, datanetflouze d
-    where c.titre = d.title
 ;
     
 insert into personne (nom, prenom)
@@ -105,24 +123,32 @@ insert into personne (nom, prenom)
 ;
 
 insert into realise (idPers, refContenu)
-    select distinct p.idPers, c.ref
+    select p.idPers, c.ref
     from contenu c, personne p, datanetflouze d, v_director vd
-    where c.titre = d.title and
+    where d.title = SUBSTR(c.titre, 1, INSTR(c.titre, ' - Saison')-1) and
           d.show_id = vd.show_id and 
           SUBSTR(vd.val, INSTR(vd.val, ' ')+1) = p.nom and  
-          SUBSTR(vd.val, 1, INSTR(vd.val, ' ')-1) = p.prenom
-          
+          SUBSTR(vd.val, 1, INSTR(vd.val, ' ')-1) = p.prenom         
+;
+
+insert into scenarise (idPers, refContenu)
+    select distinct p.idPers, c.ref
+    from contenu c, personne p, datanetflouze d, v_director vd, contenu_type ct
+    where c.titre = d.title and
+          ct.nomtype != 'Series' and
+          d.show_id = vd.show_id and 
+          SUBSTR(vd.val, INSTR(vd.val, ' ')+1) = p.nom and  
+          SUBSTR(vd.val, 1, INSTR(vd.val, ' ')-1) = p.prenom         
 ;
 
 insert into joue (idPers, refContenu)
     select distinct p.idPers, c.ref
-    from contenu c, personne p, datanetflouze d, v_cast vc, contenu_type ct
-    where ct.refContenu = c.ref and 
-          c.titre = d.title and 
-          d.show_id = vc.show_id and
+    from contenu_type ct, contenu c, personne p, datanetflouze d, v_cast vc
+    where d.show_id = vc.show_id and
+          ((c.titre = d.title and ct.nomType = 'Cinéma') or (d.title = SUBSTR(c.titre, 1, INSTR(c.titre, ' - Saison')-1) and ct.nomType = 'Series')) and
+          ct.refContenu = c.ref and 
           SUBSTR(vc.val, INSTR(vc.val, ' ')+1) = p.nom and  
-          SUBSTR(vc.val, 1, INSTR(vc.val, ' ')-1) = p.prenom and  
-          (ct.nomType = 'Cinéma' or ct.nomType = 'Series')        
+          SUBSTR(vc.val, 1, INSTR(vc.val, ' ')-1) = p.prenom         
 ;
 
 insert into compose (idPers, refContenu)
